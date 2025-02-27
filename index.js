aed8765d45dd384fee1ae02affe6e8ed6c5e356d
Chadwick Koo <chadwickkoo@outlook.com>
2025-02-27 16:09:37 +0300

    modified index.js file

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "Ashley",
+    streetAddress: "19 Wall St."
+
+};
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    const newObj = { ...obj };
+
+    newObj[key] = value;
+  
+    return newObj; 
+
+  }
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key] = value;
+
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj,key){
+        // Destructure the object, excluding the specified key
+        const { [key]: _, ...newObj } = obj;
+        return newObj; // Return a new object without the specified key
+}
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+        delete obj[key];
+        return obj
+}

dae7e3c2adfe924edf79bfd20f61d9df53099937
Chadwick Koo <chadwickkoo@outlook.com>
2025-02-27 16:13:25 +0300

    Modified index.js file

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 const employee = {
     name: "Ashley",
     streetAddress: "19 Wall St."
-
-};
+}
 function updateEmployeeWithKeyAndValue(obj,key,value){
     const newObj = { ...obj };
-
-    newObj[key] = value;
-  
-    return newObj; 
-
-  }
+    newObj[key] = value;  
+    return newObj 
+}
 function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
     obj[key] = value;
-
     return obj
 }
-
 function deleteFromEmployeeByKey(obj,key){
-        // Destructure the object, excluding the specified key
-        const { [key]: _, ...newObj } = obj;
-        return newObj; // Return a new object without the specified key
+    const { [key]: _, ...newObj } = obj;
+    return newObj
 }
 function destructivelyDeleteFromEmployeeByKey(obj,key){
-        delete obj[key];
-        return obj
+    delete obj[key];
+    return obj
 }
